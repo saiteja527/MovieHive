051351e6a17fa914de99c91e12d3e9949c7e2867
saiteja527 <boddepalli.ramana48@gmail.com>
2024-12-18 17:21:16 +0530

    save

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import { ClerkProvider } from "@clerk/clerk-react";
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 if (!PUBLISHABLE_KEY) {
-  throw new Error("Missing Publishable Key");
+  throw new Error("Missing Key");
 }
 createRoot(document.getElementById("root")).render(
   <StrictMode>
