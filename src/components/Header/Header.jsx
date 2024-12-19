56d3b96b8ea46c2c5be0dc4befb31158e3367813
saiteja527 <boddepalli.ramana48@gmail.com>
2024-12-19 22:19:56 +0530

    save

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.css";
 import { Link } from "react-router-dom";
-import { SignedIn, SignedOut, SignInButton, UserButton , useClerk } from "@clerk/clerk-react";
+import { SignedIn, SignedOut, UserButton, useClerk } from "@clerk/clerk-react";
 
 const Header = () => {
-  const { openSignIn, signOut } = useClerk();
+  const { openSignIn } = useClerk();
+  const [menuOpen, setMenuOpen] = useState(false); 
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <div className="header">
       <div className="header-left">
         <Link to="/">
-          <img src="logo.png" alt="" className="logo" />
+          <img src="logo.png" alt="Logo" className="logo" />
         </Link>
       </div>
-      <div className="header-right">
+      <div className={`header-right ${menuOpen ? "open" : ""}`}>
         <Link to="/movies/now_playing">Now Playing</Link>
         <Link to="/movies/popular">Popular</Link>
         <Link to="/movies/top_rated">Top Rated</Link>
@@ -30,6 +36,11 @@ const Header = () => {
             <UserButton className="user-button" />
           </SignedIn>
         </header>
+      </div>
+      <div className="hamburger-menu" onClick={toggleMenu}>
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
       </div>
     </div>
   );
